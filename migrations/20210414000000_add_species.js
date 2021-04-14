f220545421ebed7e7ad6eac231d43773b3eb3382
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2021-04-14 13:17:21 -0400

    Delete 20210414000000_add_species.js

--- migrations/20210414000000_add_species.js
+++
@@ -1,7 +0,0 @@
-exports.up = function(knex) {
-  return knex.raw("ALTER TABLE pets ADD species TEXT DEFAULT 'dog';")
-};
-
-exports.down = function(knex) {
-  return knex.raw("ALTER TABLE pets DROP COLUMN species;")
-};
